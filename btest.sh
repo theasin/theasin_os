3cf097b4699032e224aad44f1d38cab197f1744b
theasin <theasin32@gmail.com>
2022-06-01 22:02:12 +0300

    Удаление кода из master

--- btest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pkill qemu-system-i386
-diet i686-elf-gcc -std=gnu2x -ffreestanding -g -c start.s -o ./tmp/start.o --verbose
-i686-elf-gcc -std=gnu++20 -ffreestanding -g -c kernel.cpp -o ./tmp/kernel.o --verbose -Wall -Wextra -fno-exceptions -fno-rtti
-i686-elf-gcc -std=gnu++20 -ffreestanding -g -c kbd.cpp -o ./tmp/kbd.o --verbose -Wall -Wextra -fno-exceptions -fno-rtti
-i686-elf-gcc -std=gnu++20 -ffreestanding -g -c kutil.cpp -o ./tmp/kutil.o --verbose -Wall -Wextra -fno-exceptions -fno-rtti
-#i686-elf-gcc -ffreestanding -nostdlib -g -T linker.ld start.o kernel.o -o mykernel.elf --verbose -fno-use-linker-plugin
-i686-elf-gcc -T linker.ld -o ./mykernel.bin -ffreestanding -O2 -nostdlib ./tmp/start.o ./tmp/kernel.o ./tmp/kbd.o ./tmp/kutil.o -lgcc -fno-use-linker-plugin 
-qemu-system-i386 -kernel ./mykernel.bin
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-i686-elf-gcc -std=gnu99 -ffreestanding -g -c start.s -o start.o 
-i686-elf-c++ -std=gnu99 -ffreestanding -g -c kernel.c -o kernel.o --verbose
-i686-elf-gcc -ffreestanding -nostdlib -g -T linker.ld start.o kernel.o -o mykernel.elf --verbose -fno-use-linker-plugin
