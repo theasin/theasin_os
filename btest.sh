ea28f7ffa88003076072787cbc46b49a36592d8f
theasin <theasin32@gmail.com>
2022-07-08 21:58:30 +0300

    Исправлена кодировка скрипта btest.sh

--- btest.sh
+++ btest.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+!/bin/bash
 pkill qemu-system-i386
 i686-elf-gcc -std=gnu2x -ffreestanding -g -c start.s -o ./tmp/start.o --verbose
 i686-elf-gcc -std=gnu++20 -ffreestanding -g -c kernel.cpp -o ./tmp/kernel.o  -Wall -Wextra -fno-exceptions -fno-rtti
 i686-elf-gcc -std=gnu++20 -ffreestanding -g -c kbd.cpp -o ./tmp/kbd.o  -Wall -Wextra -fno-exceptions -fno-rtti
 i686-elf-gcc -std=gnu++20 -ffreestanding -g -c kutil.cpp -o ./tmp/kutil.o -Wall -Wextra -fno-exceptions -fno-rtti
 i686-elf-gcc -std=gnu++20 -ffreestanding -g -c display.cpp -o ./tmp/display.o -Wall -Wextra -fno-exceptions -fno-rtti
+ i686-elf-gcc -std=gnu++20 -ffreestanding -g -c mouse.cpp -o ./tmp/mouse.o  -Wall -Wextra -fno-exceptions -fno-rtti
+i686-elf-gcc -std=gnu++20 -ffreestanding -g -c kb_legacy.cpp -o ./tmp/kb_legacy.o -Wall -Wextra -fno-exceptions -fno-rtti
 #i686-elf-gcc -ffreestanding -nostdlib -g -T linker.ld start.o kernel.o -o mykernel.elf --verbose -fno-use-linker-plugin
-i686-elf-gcc -T linker.ld -o ./mykernel.bin -ffreestanding -O2 -nostdlib ./tmp/start.o ./tmp/kernel.o ./tmp/display.o ./tmp/kbd.o ./tmp/kutil.o -lgcc -fno-use-linker-plugin 
+i686-elf-gcc -T linker.ld -o ./mykernel.bin -ffreestanding -O2 -nostdlib ./tmp/start.o ./tmp/kernel.o ./tmp/display.o ./tmp/kbd.o ./tmp/kutil.o ./tmp/mouse.o ./tmp/kb_legacy.o -lgcc -fno-use-linker-plugin 
 qemu-system-i386 -kernel ./mykernel.bin
